26668b7588b4658319dad4c58a8cac33eee31861
cottons-kr <kimtaeyoung0205@naver.com>
2021-11-26 18:29:09 +0900

    commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const addButton = document.querySelector("#addButtonImg")
+const container = document.querySelector(".container")
+
+addButton.addEventListener("mouseover", () => {
+    addButton.style.opacity = 1
+})
+addButton.addEventListener("mouseout", () => {
+    addButton.style.opacity = 0.1
+})
+
+if (localStorage.length == 0) {
+    container.innerHTML = `<h1 id="noList">등록한 유튜버가 없습니다. 옆에 있는 추가 버튼으로 추가해보세요!</h1>`
+}
