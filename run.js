7977ae3523a84a06f0a3accc6475032a817ec41e
cottons-kr <kimtaeyoung0205@naver.com>
2021-11-26 19:00:21 +0900

    commit

--- run.js
+++ run.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
   let win = new BrowserWindow({
-    width: 1600,
-    height: 900,
+    width: 1920,
+    height: 1080,
     /*frame: false,*/
     webPreferences: {
       nodeIntegration: true
