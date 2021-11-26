d97204bccd467ceee4c485f9f1390e947a3f7046
cottons-kr <kimtaeyoung0205@naver.com>
2021-11-26 16:39:45 +0900

    commit

--- run.js
+++ run.js
@@ -4,11 +4,13 @@ function createWindow () {
   let win = new BrowserWindow({
     width: 800,
     height: 600,
+    /*frame: false,*/
     webPreferences: {
       nodeIntegration: true
     }
   })
   win.loadFile('html/index.html')
+  win.setMenu(null)
 }
 
 app.on('ready', createWindow);
