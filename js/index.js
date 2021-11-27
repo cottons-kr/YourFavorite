0dd8042734abe4c33b0824115fc1d736701fd11b
cottons-kr <kimtaeyoung0205@naver.com>
2021-11-27 23:36:56 +0900

    midnight commit

--- js/index.js
+++ js/index.js
@@ -6,20 +6,22 @@ const addTuberPopup = document.querySelector(".addTuberPopup")
 const addTuberPopupForm = document.querySelector("#addTuberPopupForm")
 const addTuberPopupInput = document.querySelector("#addTuberPopupForm input")
 const addTuberPopupExit = document.querySelector("#addTuberPopupExit")
+const tuberListContainer = document.querySelector("#tuberList")
+const tuberList = document.querySelector("#tuberList").children
 
-const REGEX = /[^0-9]/g
-
-function loadInfo(event) {
-    event.preventDefault()
+function loadInfo() {
     const url = addTuberPopupInput.value
     let subscriber, channelName, profileImg;
 
     addTuberPopupInput.value = ""
     const getInfo = spawn("python", ["Youtube.py", url])
     getInfo.stdout.on("data", (result) => {
-        console.log(result.toString())
+        const info = result.toString().split("::")
+        subscriber = info[0]
+        channelName = info[1]
+        profileImg = info[2]
     })
-    return subscriber;
+    return [subscriber, channelName, profileImg];
 }
 
 function addTuber(event) {
@@ -38,7 +40,7 @@ addButton.addEventListener("click", () => {
 addTuberPopupExit.addEventListener("click", () => {
     addTuberPopup.style.visibility = "hidden"
 })
-addTuberPopupForm.addEventListener("submit", loadInfo)
+addTuberPopupForm.addEventListener("submit", addTuber)
 
 if (localStorage.length == 0) {
     container.innerHTML = `<h1 id="noList">등록한 유튜버가 없습니다. 옆에 있는 추가 버튼으로 추가해보세요!</h1>`
