d08d03f1c0fe9de5be0e83f164e9ee4384a7f538
cottons-kr <kimtaeyoung0205@naver.com>
2021-11-28 15:36:09 +0900

    commit

--- getInfo.py
+++ getInfo.py
@@ -7,7 +7,7 @@ import base64
 
 def main(url, type):
     options = webdriver.ChromeOptions()
-    #options.add_argument("headless")
+    options.add_argument("headless")
     driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
     if type == "simple":
@@ -15,15 +15,7 @@ def main(url, type):
         subscriber = driver.find_element_by_xpath('''//*[@id="subscriber-count"]''').get_attribute("aria-label").split(' ')[1]
         channelName = driver.find_element_by_xpath('''//*[@id="channel-name"]''').get_attribute('innerText')
         profileImg = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/div[2]/div/div[1]/yt-img-shadow/img''').get_attribute("src")
-
-        if "천" in subscriber:
-            subscriber = subscriber.replace("천명", "a")
-        elif "만" in subscriber:
-            subscriber = subscriber.replace("만명", "b")
-        elif "억" in subscriber:
-            subscriber = subscriber.replace("억명", "c")
-
-        print(base64.b64encode(f"{subscriber}::{channelName}::{profileImg}".encode("utf-8")))
+        print(base64.b64encode(f"{subscriber.replace('명', '')}::{channelName}::{profileImg}".encode("utf-8")))
         driver.quit()
     
     elif type == "recentVideos":
@@ -33,9 +25,13 @@ def main(url, type):
         recentVideos = driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]")
         recentVideos = recentVideos.find_elements_by_tag_name("ytd-grid-video-renderer")
         for video in recentVideos:
-            print(video.find_element_by_id("video-title").get_attribute("aria-label"))
-            print(video.find_element_by_id("video-title").get_attribute("href"))
-            continue
+            videoInfo = video.find_element_by_id("video-title").get_attribute("aria-label")
+            videoLink = video.find_element_by_id("video-title").get_attribute("href")
+            videoView = videoInfo.split(" 조회수 ")[1].replace("회", '')
+            videoName = videoInfo.split(" 게시자: ")[0]
+            videoUpload = videoInfo.split(' ')[videoInfo.split(' ').index("전")-1]
+        print(base64.b64encode(f"{videoLink}::{videoName}::{videoView}::{videoUpload}".encode("utf-8")))
+        driver.quit()
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
