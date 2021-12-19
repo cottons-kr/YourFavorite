4ebaaba869a3b3c79b9387e38a404fb4561f8616
cottons-kr <kimtaeyoung0205@naver.com>
2021-12-19 23:13:27 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -87,7 +87,7 @@ function addList(channelId) {
     listChannelName.setAttribute("id", "listChannelName")
     img.setAttribute("src", info["profileImg"])
     channelButton.setAttribute("id", "channelButton")
-    channelButton.addEventListener("click", () => {showInfo(channelId)})
+    channelButton.addEventListener("click", () => {loadInfo(channelId)})
     h2.innerText = info["channelName"]
 
     listProfileImg.appendChild(img)
@@ -171,7 +171,89 @@ function removeTuber() {
     }
 }
 
-function showInfo(channelId) {
+function showInfo(info, channelId) {
+    const mainJson = JSON.parse(localStorage["youtuber"])
+    console.log(mainJson)
+    const baseInfo = JSON.parse(mainJson[channelId])
+    infoSubscriber.innerText = info["subscriber"]
+    infoProfileLink.href = baseInfo["url"]
+    infoChannelName.innerText = baseInfo["channelName"]
+    infoProfileImg.title = `${baseInfo["channelName"]}채널로 이동`
+    infoProfileImg.src = baseInfo["profileImg"]
+
+    for (let stream of info["streams"]) {
+        if (stream[1] === undefined) {
+            break
+        }
+        const div = document.createElement("div")
+        const a = document.createElement("a")
+        const img = document.createElement("img")
+        div.setAttribute("id", "stream")
+        a.setAttribute("href", stream[1])
+        a.setAttribute("id", "streamLink")
+        img.setAttribute("src", getThumbnail(stream[1]))
+        img.setAttribute("title", stream[0])
+        img.setAttribute("id", "streamThumbnail")
+        a.appendChild(img)
+        div.appendChild(a)
+        infoStreamList.appendChild(div)
+    }
+
+    for (let video of info["videos"]) {
+        if (video[1] === undefined) {
+            break
+        }
+        const div = document.createElement("div")
+        const a = document.createElement("a")
+        const img = document.createElement("img")
+        div.setAttribute("id", "video")
+        a.setAttribute("href", video[1])
+        img.setAttribute("src", getThumbnail(video[1]))
+        img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]} / ${video[2]} 전`)
+        img.setAttribute("id", "videoThumbnail")
+        a.appendChild(img)
+        div.appendChild(a)
+        infoVideosList.appendChild(div)
+    }
+
+    for (let community of info["communitys"]) {
+        if (community[0] === undefined) {
+            break
+        }
+        const div = document.createElement("div")
+        const p = document.createElement("p")
+        div.setAttribute("id", "community")
+        div.setAttribute("title", `좋아요 : ${community[1]} / ${community[2]}`)
+        p.innerText = community[0]
+        div.appendChild(p)
+        infoCommunityList.appendChild(div)
+    }
+
+    const about = info["about"]
+    infoAbout.innerText = about[0]
+    if (infoAbout.innerText.length > 15) {
+        infoAbout.innerText = `${infoAbout.innerText.substr(0, 15)}...`
+    }
+
+    infoJoinDateImg.style.visibility = "visible"
+    infoLocationImg.style.visibility = "visible"
+    infoTotalViewImg.style.visibility = "visible"
+    infoAboutImg.style.visibility = "visible"
+    infoSubscriber.style.visibility = "visible"
+    infoChannelName.style.visibility = "visible"
+    infoProfileImg.style.visibility = "visible"
+    infoTotalView.innerText = about[3]
+    infoLocation.innerText = about[1]
+    infoJoinDate.innerText = about[2]
+    infoAboutmore.innerText = about[0]
+
+    infoChannelName.style.display = "inline-block"
+    infoTuberLoading.style.display = "none"
+    infoRoot.style.display = "flex"
+    infoProfileImg.style.display = "inline-block"
+}
+
+function loadInfo(channelId) {
     loadingTuber = channelId
     showingTuber = channelId
     toggleNoList()
@@ -179,11 +261,6 @@ function showInfo(channelId) {
         clearInterval(globalInterval)
     }
 
-    infoSubscriber.innerText = ""
-    infoChannelName.innerText = ""
-    infoProfileImg.src = ""
-    infoProfileImg.style.display = "none"
-    infoChannelName.style.display = "none"
     clearInfo(channelId)
 
     const mainJson = JSON.parse(localStorage["youtuber"])
@@ -191,10 +268,6 @@ function showInfo(channelId) {
     option.args = [info["url"], "all"]
     infoTuberLoading.style.display = "block"
     infoTuberLoadingName.innerText = `${info["channelName"]} 로딩중...`
-    infoProfileImg.src = info["profileImg"]
-    infoProfileImg.title = `${info["channelName"]}채널로 이동`
-    infoProfileLink.href = info["url"]
-    infoChannelName.innerText = info["channelName"]
     PythonShell.run(rootPath+"getInfo.py", option, (error, result) => {
         if (error) {
             console.log(error)
@@ -207,83 +280,18 @@ function showInfo(channelId) {
         const buff = Buffer.from(data, "base64")
         let info = buff.toString("utf-8")
         info = JSON.parse(info)
-        
-        infoSubscriber.innerText = info["subscriber"]
+        localStorage[channelId] = JSON.stringify(info)
 
-        for (let stream of info["streams"]) {
-            if (stream[1] === undefined) {
-                break
-            }
-            const div = document.createElement("div")
-            const a = document.createElement("a")
-            const img = document.createElement("img")
-            div.setAttribute("id", "stream")
-            a.setAttribute("href", stream[1])
-            a.setAttribute("id", "streamLink")
-            img.setAttribute("src", getThumbnail(stream[1]))
-            img.setAttribute("title", stream[0])
-            img.setAttribute("id", "streamThumbnail")
-            a.appendChild(img)
-            div.appendChild(a)
-            infoStreamList.appendChild(div)
-        }
+        showInfo(info)
 
-        for (let video of info["videos"]) {
-            if (video[1] === undefined) {
-                break
-            }
-            const div = document.createElement("div")
-            const a = document.createElement("a")
-            const img = document.createElement("img")
-            div.setAttribute("id", "video")
-            a.setAttribute("href", video[1])
-            img.setAttribute("src", getThumbnail(video[1]))
-            img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]} / ${video[2]} 전`)
-            img.setAttribute("id", "videoThumbnail")
-            a.appendChild(img)
-            div.appendChild(a)
-            infoVideosList.appendChild(div)
-        }
-
-        for (let community of info["communitys"]) {
-            if (community[0] === undefined) {
-                break
-            }
-            const div = document.createElement("div")
-            const p = document.createElement("p")
-            div.setAttribute("id", "community")
-            div.setAttribute("title", `좋아요 : ${community[1]} / ${community[2]}`)
-            p.innerText = community[0]
-            div.appendChild(p)
-            infoCommunityList.appendChild(div)
-        }
-
-        const about = info["about"]
-        infoAbout.innerText = about[0]
-        if (infoAbout.innerText.length > 15) {
-            infoAbout.innerText = `${infoAbout.innerText.substr(0, 15)}...`
-        }
-        infoJoinDateImg.style.visibility = "visible"
-        infoLocationImg.style.visibility = "visible"
-        infoTotalViewImg.style.visibility = "visible"
-        infoAboutImg.style.visibility = "visible"
-
-        infoTotalView.innerText = about[3]
-        infoLocation.innerText = about[1]
-        infoJoinDate.innerText = about[2]
-        infoAboutmore.innerText = about[0]
         globalInterval = setInterval(autoRefresh, 30000)
         loadingTuber = null
-
-        infoChannelName.style.display = "inline-block"
-        infoTuberLoading.style.display = "none"
-        infoRoot.style.display = "flex"
-        infoProfileImg.style.display = "inline-block"
         localStorage["recentTuber"] = channelId
     })
     if (showingTuber !== channelId) {
         return null
     }
+    if (localStorage[channelId] !== undefined) {showInfo(JSON.parse(localStorage[channelId]), channelId)}
 }
 
 function autoRefresh() {
@@ -397,6 +405,8 @@ function clearInfo(channelId = null) {
     infoTotalViewImg.style.visibility = "hidden"
     infoLocationImg.style.visibility = "hidden"
     infoJoinDateImg.style.visibility = "hidden"
+    infoChannelName.style.visibility = "hidden"
+    infoSubscriber.style.visibility = "hidden"
     showingTuber = channelId
 }
 
@@ -429,7 +439,7 @@ function showRecentTuber() {
     if (localStorage["recentTuber"] in Object.keys(mainJson) === false) {
         localStorage["recentTuber"] = Object.keys(mainJson)[0]
     }
-    showInfo(localStorage["recentTuber"])
+    loadInfo(localStorage["recentTuber"])
 }
 
 addButtonImg.addEventListener("mouseover", () => {
