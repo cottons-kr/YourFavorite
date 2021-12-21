94eb75ccc0926f59bb850bbf8adfe8feab7eebee
cottons-kr <kimtaeyoung0205@naver.com>
2021-12-21 18:07:07 +0900

    commit

--- getInfo.py
+++ getInfo.py
@@ -7,10 +7,11 @@ while True:
         import json
         import os
         import getpass
+        from multiprocessing import Process, freeze_support, Manager
         break
     except ModuleNotFoundError:
         from subprocess import run
-        run(["powershell", ".\\resource\python-3.9.8.amd64\python -m pip install selenium msedge-selenium-tools"], shell=True)
+        run(["powershell", ".\\resource\python-3.9.8.amd64\python -m pip install selenium msedge-selenium-tools pathos"], shell=True)
         run(["powershell", ".\\resource\python-3.9.8.amd64\python -m pip install --upgrade requests"], shell=True)
         continue
 
@@ -35,8 +36,6 @@ def detectBrowser(type):
         options.add_argument("headless")
         if type == "simple":
             options.add_experimental_option("mobileEmulation", { "deviceName": "iPhone X" })
-        options.add_argument("--profile-directory=Default")
-        options.add_argument(f"user-data-dir={cachePath}\\chrome")
         driver = webdriver.Chrome(executable_path=f"{rootPath}\\chromedriver.exe", options=options)
     elif os.path.exists(edgePath):
         options = selenium_tools.EdgeOptions()
@@ -44,122 +43,154 @@ def detectBrowser(type):
             options.add_experimental_option("mobileEmulation", { "deviceName": "iPhone X" })
         options.use_chromium = True
         options.add_argument("headless")
-        options.add_argument("--profile-directory=Default")
-        options.add_argument(f"user-data-dir={cachePath}\\msedge")
         driver = selenium_tools.Edge(executable_path=f"{rootPath}\\msedgedriver.exe", options=options)
     elif os.path.exists(firefoxPath):
         options = webdriver.FirefoxOptions()
         if type == "simple":
             options.set_capability("deviceName", "iPhone X")
         options.headless = "true"
-        options.add_argument("--profile-directory=Default")
-        options.add_argument(f"user-data-dir={cachePath}\\firefox")
         driver = webdriver.Firefox(executable_path=f"{rootPath}\\geckodriver.exe", options=options)
     else:
         raise Exception("No Browser!")
     return driver
 
-def main(url, type, debug=False):
-    driver = detectBrowser(type)
+def getBase(url, returns):
+    driver = detectBrowser("all")
+    driver.get(url)
+    driver.execute_script("window.scrollTo(0, 999999999)")
+    driver.implicitly_wait(waitTime)
+    try:
+        subscriber = driver.find_element_by_xpath('''//*[@id="subscriber-count"]''').get_attribute("aria-label").split(' ')[1].replace('명', '')
+    except:
+        subscriber = "CantLoad"
+    streams = []
+    try:
+        streamDiv = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse[1]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-channel-featured-content-renderer/div[2]''')
+        streamDiv = streamDiv.find_elements_by_tag_name("ytd-video-renderer")
+    except:
+        streams = "CantLoad"
+    if streams != "CantLoad":
+        for stream in streamDiv:
+            link = stream.find_element_by_xpath('''.//*[@id="thumbnail"]''').get_attribute("href")
+            name = stream.find_element_by_xpath('''.//*[@id="video-title"]/yt-formatted-string''').get_attribute("innerText")
+            streams.append([name, link])
+    returns[0] = subscriber
+    returns[1] = streams
 
+def getVideos(url, returns):
+    driver = detectBrowser("all")
+    driver.get(url+"/videos")
+    driver.execute_script(f"window.scrollTo(0, {str(999999999 * 3)})")
+    videos = []
+    driver.implicitly_wait(waitTime)
+    try:
+        recentVideos = driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]")
+        recentVideos = recentVideos.find_elements_by_tag_name("ytd-grid-video-renderer")
+    except:
+        videos = "CantLoad"
+    if videos != "CantLoad":
+        for video in recentVideos:
+            videoInfo = video.find_element_by_id("video-title").get_attribute("aria-label")
+            videoLink = video.find_element_by_id("video-title").get_attribute("href")
+            videoView = videoInfo.split(" 조회수 ")[1].replace("회", '')
+            videoName = videoInfo.split(" 게시자: ")[0]
+            try:
+                videoUpload = videoInfo.split(' ')[videoInfo.split(' ').index("전")-1]
+            except ValueError:
+                continue
+            videos.append([videoName, videoLink, videoUpload, videoView])
+    returns[2] = videos
+
+def getCommunity(url, returns):
+    driver = detectBrowser("all")
+    driver.get(url+"/community")
+    driver.execute_script("window.scrollTo(0, 999999999)")
+    communitys = []
+    driver.implicitly_wait(waitTime)
+    try:
+        communityLogs = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-backstage-items/ytd-item-section-renderer/div[3]''')
+        communityLogs = communityLogs.find_elements_by_tag_name("ytd-backstage-post-thread-renderer")
+    except:
+        communitys = "CantLoad"
+    if communitys != "CantLoad":
+        for communityLog in communityLogs:
+            main = communityLog.find_element_by_xpath('''.//*[@id="main"]''')
+            communityUpload = main.find_element_by_xpath('''.//*[@id="published-time-text"]/a''').get_attribute('innerText')
+            communityContent = main.find_element_by_xpath('''.//*[@id="content-text"]''').get_attribute('innerText')
+            communityLikes = main.find_element_by_xpath('''.//*[@id="vote-count-middle"]''').get_attribute("innerText")
+            communitys.append([communityContent, communityLikes, communityUpload])
+    returns[3] = communitys
+
+def getAbout(url, returns):
+    driver = detectBrowser("all")
+    driver.get(url+"/about")
+    driver.execute_script("window.scrollTo(0, 999999999)")
+    driver.implicitly_wait(waitTime)
+    try:
+        about = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[1]/div[1]/yt-formatted-string[2]''').get_attribute("innerText")
+    except:
+        about = "CantLoad"
+    try:    
+        location = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[1]/div[4]/table/tbody/tr[2]/td[2]/yt-formatted-string''').get_attribute("innerText")
+    except:
+        location = "CantLoad"
+    try:
+        joinDate = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[2]/yt-formatted-string[2]/span[2]''').get_attribute("innerText").replace(' ', '')
+    except:
+        joinDate = "CantLoad"
+    try:
+        totalViews = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[2]/yt-formatted-string[3]''').get_attribute("innerText").replace("조회수 ", '').replace("회", "")
+    except:
+        totalViews = "CantLoad"
+    links = []
+    try:
+        linkDiv = driver.find_element_by_id('''link-list-container''')
+        linkDiv = linkDiv.find_elements_by_tag_name('a')
+    except:
+        links = "CantLoad"
+    if links != "CantLoad":
+        for link in linkDiv:
+            Link = link.get_attribute("href")
+            info = link.find_element_by_tag_name("yt-formatted-string").get_attribute("innerText")
+            links.append([Link, info])
+    about = [about, location, joinDate, totalViews, links]
+    returns[4] = about
+
+#=====Main=====
+if __name__ == "__main__":
+    freeze_support()
+    url = sys.argv[1]
+    type = sys.argv[2]
+    procs = []
+    manager = Manager()
+    returns = manager.dict()
+    try:
+        debug = sys.argv[3]
+    except IndexError:
+        debug = False
     if type == "simple":
+        driver  = detectBrowser(type)
         driver.get(url)
         driver.implicitly_wait(waitTime)
         channelName = driver.find_element_by_xpath('''/html/body/ytm-app/div[1]/ytm-browse/ytm-c4-tabbed-header-renderer/div[2]/div/h1''').get_attribute('innerText')
         profileImg = driver.find_element_by_xpath('''/html/body/ytm-app/div[1]/ytm-browse/ytm-c4-tabbed-header-renderer/div[2]/ytm-profile-icon/img''').get_attribute("src")
         print(base64.b64encode(f"{channelName}::{profileImg}".encode("utf-8")))
         driver.quit()
-
     elif type == "all":
-        driver.get(url)
-        driver.execute_script("window.scrollTo(0, 999999999)")
-        driver.implicitly_wait(waitTime)
-        try:
-            subscriber = driver.find_element_by_xpath('''//*[@id="subscriber-count"]''').get_attribute("aria-label").split(' ')[1].replace('명', '')
-        except:
-            subscriber = "CantLoad"
-        streams = []
-        try:
-            streamDiv = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse[1]/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer[1]/div[3]/ytd-channel-featured-content-renderer/div[2]''')
-            streamDiv = streamDiv.find_elements_by_tag_name("ytd-video-renderer")
-        except:
-            streams = "CantLoad"
-        if streams != "CantLoad":
-            for stream in streamDiv:
-                link = stream.find_element_by_xpath('''.//*[@id="thumbnail"]''').get_attribute("href")
-                name = stream.find_element_by_xpath('''.//*[@id="video-title"]/yt-formatted-string''').get_attribute("innerText")
-                streams.append([name, link])
+        procs.append(Process(target=getBase, args=(url, returns)))
+        procs.append(Process(target=getVideos, args=(url, returns)))
+        procs.append(Process(target=getCommunity, args=(url, returns)))
+        procs.append(Process(target=getAbout, args=(url, returns)))
+        for proc in procs:
+            proc.start()
+        for proc in procs:
+            proc.join()
 
-        driver.get(url+"/videos")
-        driver.execute_script(f"window.scrollTo(0, {str(999999999 * 3)})")
-        videos = []
-        driver.implicitly_wait(waitTime)
-        try:
-            recentVideos = driver.find_element_by_xpath("/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-grid-renderer/div[1]")
-            recentVideos = recentVideos.find_elements_by_tag_name("ytd-grid-video-renderer")
-        except:
-            videos = "CantLoad"
-        if videos != "CantLoad":
-            for video in recentVideos:
-                videoInfo = video.find_element_by_id("video-title").get_attribute("aria-label")
-                videoLink = video.find_element_by_id("video-title").get_attribute("href")
-                videoView = videoInfo.split(" 조회수 ")[1].replace("회", '')
-                videoName = videoInfo.split(" 게시자: ")[0]
-                try:
-                    videoUpload = videoInfo.split(' ')[videoInfo.split(' ').index("전")-1]
-                except ValueError:
-                    continue
-                videos.append([videoName, videoLink, videoUpload, videoView])
-        
-        driver.get(url+"/community")
-        driver.execute_script("window.scrollTo(0, 999999999)")
-        communitys = []
-        driver.implicitly_wait(waitTime)
-        try:
-            communityLogs = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-backstage-items/ytd-item-section-renderer/div[3]''')
-            communityLogs = communityLogs.find_elements_by_tag_name("ytd-backstage-post-thread-renderer")
-        except:
-            communitys = "CantLoad"
-        if communitys != "CantLoad":
-            for communityLog in communityLogs:
-                main = communityLog.find_element_by_xpath('''.//*[@id="main"]''')
-                communityUpload = main.find_element_by_xpath('''.//*[@id="published-time-text"]/a''').get_attribute('innerText')
-                communityContent = main.find_element_by_xpath('''.//*[@id="content-text"]''').get_attribute('innerText')
-                communityLikes = main.find_element_by_xpath('''.//*[@id="vote-count-middle"]''').get_attribute("innerText")
-                communitys.append([communityContent, communityLikes, communityUpload])
-
-        driver.get(url+"/about")
-        driver.execute_script("window.scrollTo(0, 999999999)")
-        driver.implicitly_wait(waitTime)
-        try:
-            about = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[1]/div[1]/yt-formatted-string[2]''').get_attribute("innerText")
-        except:
-            about = "CantLoad"
-        try:    
-            location = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[1]/div[4]/table/tbody/tr[2]/td[2]/yt-formatted-string''').get_attribute("innerText")
-        except:
-            location = "CantLoad"
-        try:
-            joinDate = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[2]/yt-formatted-string[2]/span[2]''').get_attribute("innerText").replace(' ', '')
-        except:
-            joinDate = "CantLoad"
-        try:
-            totalViews = driver.find_element_by_xpath('''/html/body/ytd-app/div/ytd-page-manager/ytd-browse/ytd-two-column-browse-results-renderer/div[1]/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-channel-about-metadata-renderer/div[2]/yt-formatted-string[3]''').get_attribute("innerText").replace("조회수 ", '').replace("회", "")
-        except:
-            totalViews = "CantLoad"
-        links = []
-        try:
-            linkDiv = driver.find_element_by_id('''link-list-container''')
-            linkDiv = linkDiv.find_elements_by_tag_name('a')
-        except:
-            links = "CantLoad"
-        if links != "CantLoad":
-            for link in linkDiv:
-                Link = link.get_attribute("href")
-                info = link.find_element_by_tag_name("yt-formatted-string").get_attribute("innerText")
-                links.append([Link, info])
-        about = [about, location, joinDate, totalViews, links]
-
+        subscriber = returns[0]
+        streams = returns[1]
+        videos = returns[2]
+        communitys = returns[3]
+        about  = returns[4]
         jsonData = {
             "subscriber": subscriber,
             "streams": streams,
@@ -172,10 +203,3 @@ def main(url, type, debug=False):
             print(base64.b64encode(jsonString.encode("utf-8")))
         else:
             print(jsonString)
-        driver.quit()
-
-if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        main(sys.argv[1], sys.argv[2])
-    else:
-        main(sys.argv[1], sys.argv[2], sys.argv[3])
