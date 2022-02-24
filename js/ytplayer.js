ac174c9c540209d14503fc1da7f7fafb23af424a
cottons-kr <kimtaeyoung0205@naver.com>
2022-02-24 16:22:45 +0900

    commit

--- js/ytplayer.js
+++ js/ytplayer.js
@@ -21,6 +21,29 @@ function ytPlayerExit() {
     }, 700)
 }
 
+function showInfo(video) {
+    youtubePlayer.src = `https://www.youtube-nocookie.com/embed/${video[1].split("?v=")[1]}?rel=0`
+    youtubePlayerTitle.innerText = video[0]
+    if (video[2] !== undefined) {
+        if (video[2] == "") {
+            if (lang.includes("ko")) {youtubePlayerInfo.innerText = `조회수 : ${video[3]}`}
+            else if (lang.includes("ko")) {youtubePlayerInfo.innerText = `ヒット : ${video[3]}`}
+            else {youtubePlayerInfo.innerText = `Views : ${video[3]}`}
+        }
+        else {
+            if (lang.includes("ko")) {youtubePlayerInfo.innerText = `조회수 : ${video[3]} / ${video[2]} 전`}
+            else if (lang.includes("ko")) {youtubePlayerInfo.innerText = `ヒット : ${video[3]} / ${video[2]}前`}
+            else {youtubePlayerInfo.innerText = `Views : ${video[3]} / ${video[2]} ago`}
+        }
+    } else {
+        if (video[3] !== undefined) {
+            if (lang.includes("ko")) {youtubePlayerInfo.innerText = `조회수 : ${video[3]}`}
+            else if (lang.includes("ko")) {youtubePlayerInfo.innerText = `ヒット : ${video[3]}`}
+            else {youtubePlayerInfo.innerText = `Views : ${video[3]}`}
+        } else {youtubePlayerInfo.innerText = ""}
+    }
+}
+
 function getThumbnail(url) {
     return (`https://i.ytimg.com/vi/${url.replace(/^.*(?:youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/g,"$1")}/original.jpg`)
 }
@@ -32,38 +55,32 @@ export default function loadPlayer(data, channelVideos) {
     youtubePlayer.removeAttribute("style")
     listContainer.style.pointerEvents = "none"
     container.style.pointerEvents = "none"
-    youtubePlayer.src = `https://www.youtube-nocookie.com/embed/${data[1].split("?v=")[1]}?rel=0`
-    youtubePlayerTitle.innerText = data[0]
-    if (data[2] !== undefined) {
-        if (lang.includes("ko")) {youtubePlayerInfo.innerText = `조회수 : ${data[3]} / ${data[2]} 전`}
-        else if (lang.includes("ko")) {youtubePlayerInfo.innerText = `ヒット : ${data[3]} / ${data[2]}前`}
-        else {youtubePlayerInfo.innerText = `Views : ${data[3]} / ${data[2]} ago`}
-    }
+    showInfo(data)
 
     for (let video of channelVideos) {
         const div = document.createElement("div")
         const img = document.createElement("img")
         div.setAttribute("id", "youtubePlayerVideo")
         img.setAttribute("src", getThumbnail(video[1]))
-        if (video[2] == "") {
-            if (lang.includes("ko")) {img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]}`)}
-            else if (lang.includes("jp")) {img.setAttribute("title", `${video[0]} /  ヒット: ${video[3]}`)}
-            else {img.setAttribute("title", `${video[0]} / Views : ${video[3]}`)}
+        if (video[2] !== undefined) {
+            if (video[2] == "") {
+                if (lang.includes("ko")) {img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]}`)}
+                else if (lang.includes("jp")) {img.setAttribute("title", `${video[0]} / ヒット : ${video[3]}`)}
+                else {img.setAttribute("title", `${video[0]} / Views : ${video[3]}`)}
+            } else {
+                if (lang.includes("ko")) {img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]} / ${video[2]} 전`)}
+                else if (lang.includes("jp")) {img.setAttribute("title", `${video[0]} / ヒット : ${video[3]} / ${video[2]} 前`)}
+                else {img.setAttribute("title", `${video[0]} / Views : ${video[3]} / ${video[2]} ago`)}
+            }
         } else {
-            if (lang.includes("ko")) {img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]} / ${video[2]} 전`)}
-            else if (lang.includes("jp")) {img.setAttribute("title", `${video[0]} / ヒット : ${video[3]} / ${video[2]} 前`)}
-            else {img.setAttribute("title", `${video[0]} / Views : ${video[3]} / ${video[2]} ago`)}
+            if (video[3] !== undefined) {
+                if (lang.includes("ko")) {img.setAttribute("title", `${video[0]} / 조회수 : ${video[3]}`)}
+                else if (lang.includes("jp")) {img.setAttribute("title", `${video[0]} /  ヒット: ${video[3]}`)}
+                else {img.setAttribute("title", `${video[0]} / Views : ${video[3]}`)}
+            } else {img.setAttribute("title", video[0])}
         }
 
-        div.addEventListener("click", () => {
-            youtubePlayer.src = `https://www.youtube-nocookie.com/embed/${video[1].split("?v=")[1]}?fs=0&rel=0`
-            youtubePlayerTitle.innerText = video[0]
-            if (video[2] !== undefined) {
-                if (lang.includes("ko")) {youtubePlayerInfo.innerText = `조회수 : ${video[3]} / ${video[2]} 전`}
-                else if (lang.includes("ko")) {youtubePlayerInfo.innerText = `ヒット : ${video[3]} / ${video[2]}前`}
-                else {youtubePlayerInfo.innerText = `Views : ${video[3]} / ${video[2]} ago`}
-            }
-        })
+        div.addEventListener("click", () => {showInfo(video)})
         img.setAttribute("id", "videoThumbnail")
         div.appendChild(img)
         youtubePlayerVideoList.appendChild(div)
